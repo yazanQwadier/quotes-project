f2a73505f9f24963f740c12df7fbeaa5e0eb24d7
yazanQwaider <yazeno15@gmail.com>
2022-06-11 19:00:16 +0300

    Add MiniCssExtractPlugin to apply css on elements directly

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'production',
@@ -7,13 +8,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js',
         assetModuleFilename: 'assets/[name][ext]',
+        
+        asyncChunks: true,
     },
     module: {
         rules: [
           {
             test: /\.s[ac]ss$/i,
             use: [
-                "style-loader","css-loader",
+                // "style-loader",
+                MiniCssExtractPlugin.loader,
+                "css-loader",
                 {
                     loader: "sass-loader",
                     options: {
@@ -24,5 +29,12 @@ module.exports = {
           },
         ]
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        })
+    ],
 };
 
